720a304b5f167db3d3e0db02057e95f2a4b0b849
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-03-03 11:10:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-const somaHorasExtras = (salario, valorHorasExtras) => {
-    return salario + valorHorasExtras;
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const verifiqueSe = (valor) => {
+    const assercoes = {
+        ehExatamenteIgualA(esperado){
+            if(valor !==esperado){
+                throw{};
+            }
+        },
+    };
+    return assercoes;
 }
-console.log(somaHorasExtras(2000,500));
+const teste = (titulo, funcaoDeTeste) => {
+    try{
+funcaoDeTeste();
+console.log('${titulo} passou!')
+    }
+catch{
+    console.error('${titulo} não passou !!!')
+}
+
+};
+
+teste('somaHorasExtras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras (2000,500);
+    verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
+
+teste('calculaDesconto', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos (2500,200);
+    verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
